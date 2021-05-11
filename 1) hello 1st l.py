adf8512ba364fab18a991f4f6ce8f470316d220f
protonzener <70801862+protonzener@users.noreply.github.com>
2021-05-11 21:53:33 +0530

    Add files via upload

---
+++ 1) hello 1st l.py
@@ -0,0 +1,23 @@
+print("Hello World",end=" ")
+
+print("""\nHello
+world
+____
+____
+""")
+#this is single line comment 
+
+"""this
+ is multiline 
+comment
+"""
+print("I am  proton from c:\\numpy\\downloads","hello.py",end="")
+
+"""
+escape sequence -
+\n - new line
+\t - tab
+
+and to avoid put double \\
+
+"""
---
+++ 2) variables and datatypes.py
@@ -0,0 +1,14 @@
+var1 = "hello world "
+var2 = 45
+var3 = 434.43
+var4 = " code "
+var5 = "34"
+print(var1)
+print(type(var3))
+print(var3)
+
+print(var1 + var4)
+
+# typecasting - changing types
+print(int(var5) + var2)
+print(10 * var3)
