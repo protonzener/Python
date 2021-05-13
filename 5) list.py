d3aeb629e97785ad27f0d461df03348f76018ada
protonzener <70801862+protonzener@users.noreply.github.com>
2021-05-13 14:42:34 +0530

    Add files via upload

---
+++ 5) list.py
@@ -0,0 +1,51 @@
+mylist = ["carrot","cat","bee","honey",34]
+nums = [1,2,34,324,45,6,7]
+print(mylist)
+print(nums)
+print("\n")
+
+#Accessing elements of list
+print(mylist[2])
+print(nums[3])
+print("\n")
+
+#sorting
+print(nums.sort())
+print(nums)
+print("\n")
+
+#reversing
+print(nums.reverse())
+print(nums)
+print("\n")
+
+# max and min
+print(max(nums))
+print(min(nums))
+
+
+#append (adding at last of list)
+add =[]
+add.append(43)
+print(add)
+add.append(42)
+print(add)
+add.append(41)
+print(add)
+
+#for particular element
+add.remove(42)
+print(add)
+
+#deletes last element
+add.pop()
+print(add)
+
+#insert
+add.insert(2,"insert")
+print(add)
+
+
+
+
+
---
+++ 6)tuple.py
@@ -0,0 +1,16 @@
+#tuples are immutable(can not change)
+tp = (1,2,3,4,2,2)
+print(tp)
+
+#counting
+print(tp.count(2))
+
+
+#to swap
+a =1
+b=2
+print(a,b)
+
+a,b=b,a
+print(a,b)
+
